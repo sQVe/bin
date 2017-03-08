d2656a9232e631bd2b6ba5ab0a8c0f4c2cf2f211
Oskar Grunning <oskargrunning@gmail.com>
2017-03-08 06:06:25 +0100

    Add start script for chromium

---
+++ chromium-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+chromium --remote-debugging-port=9222
--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
+ln -s /home/sqve/.bin/chromium-start.sh /usr/local/bin/chromium-start
 ln -s /home/sqve/.bin/gitkraken-start.sh /usr/local/bin/gitkraken-start
 ln -s /home/sqve/.bin/i3exit.sh /usr/local/bin/i3exit
 ln -s /home/sqve/.bin/load-icc.sh /usr/local/bin/load-icc
 ln -s /home/sqve/.bin/lock.sh /usr/local/bin/lock
 ln -s /home/sqve/.bin/postman.sh /usr/local/bin/postman
 ln -s /home/sqve/.bin/spotify-start.sh /usr/local/bin/spotify-start
-
-ln -s /home/sqve/.bin/private/twitch/index.coffee /usr/local/bin/twitch
