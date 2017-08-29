eb9bbba6609d706e843ea304113209e395b1baf9
Oskar Grunning <oskargrunning@gmail.com>
2017-08-29 18:42:14 +0200

    Repair ICC loading

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 ln -s /home/sqve/.bin/chromium-start.sh /usr/local/bin/chromium-start
 ln -s /home/sqve/.bin/i3exit.sh /usr/local/bin/i3exit
-ln -s /home/sqve/.bin/load-icc.sh /usr/local/bin/load-icc
 ln -s /home/sqve/.bin/lock.sh /usr/local/bin/lock
 ln -s /home/sqve/.bin/postman.sh /usr/local/bin/postman
 ln -s /home/sqve/.bin/spotify-start.sh /usr/local/bin/spotify-start
