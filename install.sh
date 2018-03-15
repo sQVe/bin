9e1e53e4fdff217cf83f21450908502205e8b806
Oskar Grunning <oskargrunning@gmail.com>
2018-03-15 22:36:20 +0100

    Rename spotify launcher

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -s /home/sqve/.bin/lock.sh /usr/local/bin/lock
 ln -s /home/sqve/.bin/mount-shishigami.sh /usr/local/bin/mount-shishigami
 ln -s /home/sqve/.bin/ohoy.sh /usr/local/bin/ohoy
 ln -s /home/sqve/.bin/phone.sh /usr/local/bin/phone
-ln -s /home/sqve/.bin/spotify-start.sh /usr/local/bin/spotify-start
+ln -s /home/sqve/.bin/start-spotify.sh /usr/local/bin/start-spotify
 ln -s /home/sqve/.bin/start-qutebrowser.sh /usr/local/bin/start-qutebrowser
 ln -s /home/sqve/.bin/t.sh /usr/local/bin/t
 ln -s /home/sqve/.bin/sx.sh /usr/local/bin/sx
