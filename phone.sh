377b26922435e616f24fa4567a34587d3f22f542
Oskar Grunning <oskargrunning@gmail.com>
2018-04-15 22:01:13 +0200

    Enable phone script to print help

--- phone.sh
+++ phone.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-
-if [[ $# -eq 0 ]] ; then
+if [[ "$#" -eq 0 ]] ; then
   echo "No arguments supplied!"
   exit 1
 fi
 
+while [[ "$#" -gt 0 ]]; do
+  if [[ $1 =~ ^-h|--help|help$ ]]; then
+    kdeconnect-cli --help
+    exit 0
+  fi
+  shift
+done
+
 device="HUAWEI"
 
-id=$(
+id="$(
   (kdeconnect-cli -a | ag "$device:" | awk '{print $3}') ||
   (kdeconnect-cli -l | ag "$device:" | awk '{print $3}')
-)
+)"
 
 if [[ -z "$id" ]]; then
   echo "Phone was not found!"
