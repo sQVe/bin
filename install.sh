968e16c36ce5243f194001b672d977ff63039611
Oskar Grunning <oskargrunning@gmail.com>
2018-06-15 11:22:11 +0200

    Remove ohoy deploy script

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@
 
 ln -s /home/sqve/.bin/lock.sh /usr/local/bin/lock
 ln -s /home/sqve/.bin/mount-shishigami.sh /usr/local/bin/mount-shishigami
-ln -s /home/sqve/.bin/ohoy.sh /usr/local/bin/ohoy
 ln -s /home/sqve/.bin/phone.sh /usr/local/bin/phone
 ln -s /home/sqve/.bin/scaled-spotify.sh /usr/local/bin/scaled-spotify
 ln -s /home/sqve/.bin/scaled-qutebrowser.sh /usr/local/bin/scaled-qutebrowser
