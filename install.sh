b147b3a981d0a3dc3b581bea51adb69c5963a356
Oskar Grunning <oskargrunning@gmail.com>
2018-09-06 09:51:30 +0200

    Add random wallpaper script

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -s /home/sqve/.bin/lock.sh /usr/local/bin/lock
 ln -s /home/sqve/.bin/mount-animech-shares.sh /usr/local/bin/mount-animech-shares
 ln -s /home/sqve/.bin/mount-shishigami.sh /usr/local/bin/mount-shishigami
 ln -s /home/sqve/.bin/phone.sh /usr/local/bin/phone
+ln -s /home/sqve/.bin/random-wallpaper.sh /usr/local/bin/random-wallpaper
 ln -s /home/sqve/.bin/scaled-qutebrowser.sh /usr/local/bin/scaled-qutebrowser
 ln -s /home/sqve/.bin/scaled-spotify.sh /usr/local/bin/scaled-spotify
 ln -s /home/sqve/.bin/sx.sh /usr/local/bin/sx
