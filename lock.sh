9596b2bd31ee79c56186e9f1a488572e2ddb7c27
Oskar Grunning <oskargrunning@gmail.com>
2019-03-22 15:17:59 +0100

    Refactor lock utility

--- lock.sh
+++ lock.sh
@@ -4,10 +4,13 @@
 #  ┃  ┃ ┃┃  ┣┻┓
 #  ┗━╸┗━┛┗━╸╹ ╹
 
-playerctl pause
-i3lock -c 22093F
-gpg-connect-agent reloadagent /bye
+(sleep 30s && xset dpms force standby) &
+dpms_standby=$!
 
-sleep 30 && \
-  pgrep i3lock && \
-  xset dpms force standby
+(sleep 30m && gpg-connect-agent reloadagent /bye) &
+gpg_clear=$!
+
+playerctl pause
+i3lock -n -c 22093F
+kill $dpms_standby
+kill $gpg_clear
