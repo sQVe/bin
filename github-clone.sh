acea1137b8f995751b79f6d03d6e72d231627d40
Oskar Grunning <oskargrunning@gmail.com>
2019-04-22 21:02:57 +0200

    Make github-clone more flexible

--- github-clone.sh
+++ github-clone.sh
@@ -4,14 +4,34 @@
 #  ┃╺┓┃ ┃ ┣━┫┃ ┃┣┻┓   ┃  ┃  ┃ ┃┃┗┫┣╸
 #  ┗━┛╹ ╹ ╹ ╹┗━┛┗━┛   ┗━╸┗━╸┗━┛╹ ╹┗━╸
 
+cut() {
+  command cut -d '/' -f "$1" <<< "$2"
+}
+
+is_url() {
+  rg '^http' <<< "$1"
+}
+
+is_identifier() {
+  rg '/' <<< "$1"
+}
+
 if [[ $# -eq 1 ]]; then
-  name="sQVe"
-  repository=$1
+  if [[ $(is_url "$1") ]]; then
+    identifier=$(cut 4-5 "$1")
+  elif [[ $(is_identifier "$1") ]]; then
+    identifier="$1"
+  else
+    identifier="sQVe/$1"
+  fi
+
+  name=$(cut 1 "$identifier")
+  repository=$(cut 2 "$identifier")
 elif [[ $# -eq 2 ]]; then
   name=$1
   repository=$2
 else
-  exit 0
+  echo "No valid arguments. Exiting." && exit 0
 fi
 
 git clone "git@github.com:$name/$repository.git"
