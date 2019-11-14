c6da3ed0a304837d345efba96c1542a8eab763d6
Oskar Grunning <oskargrunning@gmail.com>
2019-11-14 13:53:41 +0100

    Repair Animech share mount

--- mount-animech-shares.sh
+++ mount-animech-shares.sh
@@ -15,28 +15,26 @@ sudo mkdir -p /mnt/animech/misc
 sudo mkdir -p /mnt/animech/projects
 sudo mkdir -p /mnt/animech/support
 
-
-echo -n 'AD Password: '
-read -rs password
+password=$(pass "animech/active-directory/OsGr")
 
 echo -e "${cyan}Mounting animech shares...${nc}"
 
 echo -n 'administration: '
-sudo mount -t cifs //animech-shares/Administration /mnt/animech/administration -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8
+sudo mount -t cifs //animech-shares/Administration /mnt/animech/administration -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8,vers=2.1
 echo -e "${green}OK${nc}"
 
 echo -n 'development: '
-sudo mount -t cifs //animech-shares/development /mnt/animech/development -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8
+sudo mount -t cifs //animech-shares/development /mnt/animech/development -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8,vers=2.1
 echo -e "${green}OK${nc}"
 
 echo -n 'misc: '
-sudo mount -t cifs //animech-shares/Misc /mnt/animech/misc -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8
+sudo mount -t cifs //animech-shares/Misc /mnt/animech/misc -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8,vers=2.1
 echo -e "${green}OK${nc}"
 
 echo -n 'projects: '
-sudo mount -t cifs //animech-shares/Projects /mnt/animech/projects -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8
+sudo mount -t cifs //animech-shares/Projects /mnt/animech/projects -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8,vers=2.1
 echo -e "${green}OK${nc}"
 
 echo -n 'support: '
-sudo mount -t cifs //animech-shares/Support /mnt/animech/support -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8
+sudo mount -t cifs //animech-shares/Support /mnt/animech/support -o username=OsGr,password="$password",workgroup=AnimechDomain.local,iocharset=utf8,vers=2.1
 echo -e "${green}OK${nc}"
