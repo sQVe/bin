ca39b8dde7145bc2ee4b544ecbee6d4f00e9b854
Oskar Grunning <oskargrunning@gmail.com>
2020-01-23 21:49:18 +0100

    Repair git alias lookup script after zim update

--- lookup-git-alias.sh
+++ lookup-git-alias.sh
@@ -9,16 +9,18 @@ if [[ $# -eq 0 ]]; then
   exit 0
 fi
 
-matches=$(rg '^\s+alias' <"$ZIM_HOME"/modules/git/init.zsh | rg --case-sensitive "$1")
+matches=$(rg '^\s+alias' <"$ZIM_HOME"/modules/git/init.zsh | sed 's/${gprefix}/g/' | rg --case-sensitive "$1")
 result=""
 
 while read -r line; do
   alias=$(echo "$line" | sed -r 's/^alias (\w+).*/\1/')
   cmd=$(echo "$line" | cut -d '=' -f 2- | sed -r "s/'//g")
-  description=$(rg --case-sensitive "^\s+\* \`$alias\`" \
-    <"$ZIM_HOME"/modules/git/README.md |
-    rg -v "\` shadows" |
-    cut -d ' ' -f 5-)
+  description=$(
+    sed 's/`G/`g/' "$ZIM_HOME"/modules/git/README.md |
+      rg --case-sensitive "^\s+\* \`$alias\`" |
+      rg -v "\` shadows" |
+      cut -d ' ' -f 5-
+  )
 
   if [[ -n "$alias" && -n "$cmd" ]]; then
     result="$result\n$alias%%$cmd%%$description"
