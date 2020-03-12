2880e51ca8714886f4e910ab3de2ec3cfa780846
Oskar Grunning <oskargrunning@gmail.com>
2020-03-12 23:48:59 +0100

    Add nmgui

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ ln -s /home/sqve/.bin/github-clone.sh /usr/local/bin/github-clone
 ln -s /home/sqve/.bin/laptop-keyboard-toggle.sh /usr/local/bin/laptop-keyboard-toggle
 ln -s /home/sqve/.bin/lock.sh /usr/local/bin/lock
 ln -s /home/sqve/.bin/lookup-git-alias.sh /usr/local/bin/lookup-git-alias
+ln -s /home/sqve/.bin/mons-work.sh /usr/local/bin/mons-work
 ln -s /home/sqve/.bin/mount-shishigami.sh /usr/local/bin/mount-shishigami
+ln -s /home/sqve/.bin/nmgui.sh /usr/local/bin/nmgui
 ln -s /home/sqve/.bin/note.sh /usr/local/bin/note
 ln -s /home/sqve/.bin/nvim-dir.sh /usr/local/bin/nvim-dir
 ln -s /home/sqve/.bin/phone.sh /usr/local/bin/phone
@@ -18,7 +20,6 @@ ln -s /home/sqve/.bin/repos-behind.sh /usr/local/bin/repos-behind
 ln -s /home/sqve/.bin/repos-dirty.sh /usr/local/bin/repos-dirty
 ln -s /home/sqve/.bin/scaled-qutebrowser.sh /usr/local/bin/scaled-qutebrowser
 ln -s /home/sqve/.bin/scaled-spotify.sh /usr/local/bin/scaled-spotify
-ln -s /home/sqve/.bin/mons-work.sh /usr/local/bin/mons-work
 ln -s /home/sqve/.bin/sx.sh /usr/local/bin/sx
 ln -s /home/sqve/.bin/term.sh /usr/local/bin/term
 ln -s /home/sqve/.bin/uberfm.sh /usr/local/bin/uberfm
---
+++ nmgui.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#  ┏┓╻┏┳┓┏━╸╻ ╻╻
+#  ┃┗┫┃┃┃┃╺┓┃ ┃┃
+#  ╹ ╹╹ ╹┗━┛┗━┛╹
+
+(nm-applet) &
+nm_applet_background=$!
+
+stalonetray --background "#282828" >/dev/null 2>&1
+kill $nm_applet_background
