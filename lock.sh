7238b3032d125e48e4d9e297f4901e6b7d424a3a
Oskar Grunning <oskargrunning@gmail.com>
2021-03-05 08:08:05 +0100

    Remove greenclip usage

--- lock.sh
+++ lock.sh
@@ -7,15 +7,11 @@
 (sleep 1m && xset dpms force standby) &
 dpms_standby=$!
 
-(sleep 15m && pkill greenclip && greenclip clear && greenclip daemon) &
-clipboard_clear=$!
-
-(sleep 30m && gpg-connect-agent reloadagent /bye) &
+(sleep 15m && gpg-connect-agent reloadagent /bye) &
 gpg_clear=$!
 
 playerctl pause &
 i3lock -n -c 3c3836
 
 kill $dpms_standby
-kill $clipboard_clear
 kill $gpg_clear
