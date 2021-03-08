d5174b7377a206c615ca91224a6aae645bc2b03e
Oskar Grunning <oskargrunning@gmail.com>
2021-03-08 15:21:37 +0100

    Pause dunst on system lock

--- lock.sh
+++ lock.sh
@@ -11,7 +11,10 @@ dpms_standby=$!
 gpg_clear=$!
 
 playerctl pause &
+
+notify-send "DUNST_COMMAND_PAUSE"
 i3lock -n -c 3c3836
+notify-send "DUNST_COMMAND_RESUME"
 
 kill $dpms_standby
 kill $gpg_clear
