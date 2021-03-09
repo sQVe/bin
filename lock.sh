2616ac219c9a473fba6ea1394936f2a0ca3f2566
Oskar Grunning <oskargrunning@gmail.com>
2021-03-09 16:40:42 +0100

    Handle dpms with lock bin

--- lock.sh
+++ lock.sh
@@ -14,11 +14,13 @@ playerctl pause &
 
 dunstify "DUNST_COMMAND_PAUSE"
 pkill xcompmgr
+xset dpms 30 30 30
 
 i3lock -n -c 3c3836
 
 dunstify "DUNST_COMMAND_RESUME"
 xcompmgr -C &
+xset dpms 240 240 240
 
 kill $dpms_standby
 kill $gpg_clear
