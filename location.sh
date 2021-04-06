14e929ff9c717b1ee8592d5739c873cffe081e05
Oskar Grunning <oskargrunning@gmail.com>
2021-04-06 11:04:39 +0200

    Make location home turn off screen pre setup

--- location.sh
+++ location.sh
@@ -6,6 +6,8 @@
 
 case "$1" in
   home)
+    xrandr --output DP-1-2 --off --output DP-1-3 --off
+    sleep 3
     xrandr --output eDP-1 --mode 1920x1080 --pos 4000x681 --rotate normal \
       --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate left \
       --output DP-1-3 --primary --mode 2560x1440 --pos 1440x501 --rotate normal
