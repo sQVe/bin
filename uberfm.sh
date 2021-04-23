dcab36d9f340dca7692c7a86ed1c82d171070ce1
Oskar Grunning <oskargrunning@gmail.com>
2021-04-23 08:51:06 +0200

    Update uberfm bin

--- uberfm.sh
+++ uberfm.sh
@@ -4,19 +4,20 @@
 # ┃ ┃┣┻┓┣╸ ┣┳┛┣╸ ┃┃┃
 # ┗━┛┗━┛┗━╸╹┗╸╹  ╹ ╹
 
-if [ -z "$(whereis ueberzug | awk '{print $2}')" ]; then
-  exec vifm --choose-dir - "$@" && exit
-elif [ -z "$DISPLAY" ]; then
-  exec vifm --choose-dir - "$@" && exit
-else
-  cleanup() {
-    rm "$FIFO_UEBERZUG"
-    pkill -P $$ > /dev/null
-  }
-  [ ! -d "$HOME/.cache/vifm" ] && mkdir --parents "$HOME/.cache/vifm"
-  export FIFO_UEBERZUG="$HOME/.cache/vifm/ueberzug-${PPID}"
-  mkfifo "$FIFO_UEBERZUG"
-  tail --follow "$FIFO_UEBERZUG" | ueberzug layer --silent --parser bash 2>&1 > /dev/null &
-  trap cleanup EXIT
-  vifm --choose-dir - "$@"
-fi
+test -z "$(which ueberzug)" \
+  && exec vifm --choose-dir - "$@" && exit
+
+test -z "$DISPLAY" \
+  && exec vifm --choose-dir - "$@" && exit
+
+cleanup() {
+  rm "$FIFO_UEBERZUG"
+  pkill -P $$ > /dev/null
+}
+
+! test -d "$HOME/.cache/vifm" && mkdir -p "$HOME/.cache/vifm"
+export FIFO_UEBERZUG="$HOME/.cache/vifm/ueberzug-${PPID}"
+mkfifo "$FIFO_UEBERZUG"
+tail --follow "$FIFO_UEBERZUG" | ueberzug layer --silent --parser bash > /dev/null 2>&1 &
+trap cleanup EXIT
+vifm --choose-dir - "$@"
