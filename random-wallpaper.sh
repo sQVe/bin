b01b34829aa6358d4221f90fa5ab4f5143110c79
Oskar Grunning <oskargrunning@gmail.com>
2021-05-06 08:13:27 +0200

    Tweak random wallpaper script

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -4,4 +4,11 @@
 #  ┣┳┛┣━┫┃┗┫ ┃┃┃ ┃┃┃┃   ┃╻┃┣━┫┃  ┃  ┣━┛┣━┫┣━┛┣╸ ┣┳┛
 #  ╹┗╸╹ ╹╹ ╹╺┻┛┗━┛╹ ╹   ┗┻┛╹ ╹┗━╸┗━╸╹  ╹ ╹╹  ┗━╸╹┗╸
 
-feh --randomize --bg-fill --no-fehbg "$PICTURES"/wallpapers/*
+function get_random_directory() {
+  fd . --type d --max-depth 1 "$1" | shuf -n 1
+}
+
+source=$(get_random_directory "$PICTURES/wallpapers")
+random_category=$(get_random_directory "$source")
+
+[[ -n "$random_category" ]] && feh --randomize --bg-fill --no-fehbg "$random_category"/*
