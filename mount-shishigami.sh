d562da0e4efc48b3526afe58facfa9847e8c07c6
Oskar Grunning <oskargrunning@gmail.com>
2021-05-18 20:52:50 +0200

    chore: remove portable-hdd mount

--- mount-shishigami.sh
+++ mount-shishigami.sh
@@ -12,7 +12,6 @@ sudo mkdir -p /mnt/shishigami
 sudo mkdir -p /mnt/shishigami/media
 sudo mkdir -p /mnt/shishigami/misc
 sudo mkdir -p /mnt/shishigami/pictures
-sudo mkdir -p /mnt/shishigami/portable-hdd
 
 echo -e "${cyan}Mounting shishigami...${nc}"
 
@@ -26,8 +25,4 @@ echo -e "${green}OK${nc}"
 
 echo -n 'pictures: '
 sudo mount -t nfs -o vers=4 shishigami:/volume1/pictures /mnt/shishigami/pictures
-echo -e "${green}OK${nc}"
-
-echo -n 'portable-hdd: '
-sudo mount -t nfs -o vers=4 shishigami:/volumeUSB1/usbshare /mnt/shishigami/portable-hdd
 echo -e "${green}OK${nc}"
