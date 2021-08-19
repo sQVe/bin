7a774d273c58a44d03688d15e9129dbd3af8a5e3
Oskar Grunning <oskargrunning@gmail.com>
2021-08-19 08:22:56 +0200

    chore: change name on location bin

--- mons-work.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#  ╻ ╻┏━┓┏━┓╻┏    ┏┳┓┏━┓┏┓╻┏━┓
-#  ┃╻┃┃ ┃┣┳┛┣┻┓   ┃┃┃┃ ┃┃┗┫┗━┓
-#  ┗┻┛┗━┛╹┗╸╹ ╹   ╹ ╹┗━┛╹ ╹┗━┛
-
-xrandr --output DP-1 --auto --rotate right --output DP-2 --auto --primary --rotate right --right-of DP-1 --output eDP-1 --auto --right-of DP-2
-
-random-wallpaper
