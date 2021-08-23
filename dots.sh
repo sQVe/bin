42baabe38261dd39d7f4affbd7394f831bcdfab3
Oskar Grunning <oskargrunning@gmail.com>
2021-08-23 17:13:09 +0200

    chore: add preview to dots bin

--- dots.sh
+++ dots.sh
@@ -4,8 +4,8 @@
 #   ┃┃┃ ┃ ┃ ┗━┓
 #  ╺┻┛┗━┛ ╹ ┗━┛
 
-files="$(fd --base-directory "$DOTFILES" --type 'file')"
-choice="$(fzf --select-1 --query "$*" <<< "$files" || exit)"
+files="$(fd --absolute-path --base-directory "$DOTFILES" --type 'file')"
+choice="$(fzf --preview 'bat --color=always {} 2> /dev/null' --preview-window 'down:50%' --select-1 --query "$*" <<< "$files" || exit)"
 
 [[ -z "$choice" ]] && exit 1
 
