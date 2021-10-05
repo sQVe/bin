aee892b2a883e35ed0ef7c0b070e8ea095b32f5a
Oskar Grunning <oskargrunning@gmail.com>
2021-10-05 08:50:07 +0200

    chore: setup new home monitor layout

--- mons-layout.sh
+++ mons-layout.sh
@@ -8,10 +8,8 @@ case "$1" in
   home)
     xrandr --output DP-1-2 --off --output DP-1-3 --off
     sleep 3
-    xrandr --output eDP-1 --mode 1920x1080 --pos 4000x740 --rotate normal \
-      --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate left \
-      --output DP-1-3 --primary --mode 2560x1440 --pos 1440x560 --rotate normal
 
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2560x766 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal
     random-wallpaper
     ;;
 esac
