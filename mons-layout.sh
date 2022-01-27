f24c1d149bdae271ea090ed0cb87f0291b86d408
Oskar Grunning <oskargrunning@gmail.com>
2022-01-27 08:39:00 +0100

    chore: change primary screen on home mons layout

--- mons-layout.sh
+++ mons-layout.sh
@@ -9,7 +9,7 @@ case "$1" in
     xrandr --output DP-1-2 --off --output DP-1-3 --off
     sleep 3
 
-    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2560x766 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal
+    xrandr --output eDP-1 --mode 1920x1080 --pos 2560x766 --rotate normal --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal
     random-wallpaper
     ;;
 esac
