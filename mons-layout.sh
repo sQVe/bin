22d6f5f0a1d50f71e7d2e2bed54f757eb04f3538
Oskar Grunning <oskargrunning@gmail.com>
2022-06-27 09:12:38 +0200

    chore: tweak home mons setup

--- mons-layout.sh
+++ mons-layout.sh
@@ -8,8 +8,7 @@ case "$1" in
   home)
     xrandr --output DP-1-2 --off --output DP-1-3 --off
     sleep 3
-
-    xrandr --output eDP-1 --mode 1920x1080 --pos 2560x766 --rotate normal --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal
+    xrandr --output eDP-1 --mode 1920x1080 --pos 2560x561 --rotate normal --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal
     random-wallpaper
     ;;
 esac
