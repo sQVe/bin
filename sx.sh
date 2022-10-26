748799afaca2c8c8ced1b606071ac19fb443def5
Oskar Grunning <oskargrunning@gmail.com>
2022-10-26 12:09:16 +0200

    chore: update sx bin

--- sx.sh
+++ sx.sh
@@ -5,20 +5,20 @@
 #  ┗━┛╹ ╹
 
 # Ensure Xorg starts in home directory.
-cd "$HOME" || exit
+cd "${HOME}" || exit
 
 # Touch Xauthority to remove "file not found" outputs.
-touch "$XDG_RUNTIME_DIR/Xauthority"
+touch "${XDG_RUNTIME_DIR}/.Xauthority"
 
 if [[ $1 =~ ^-?[0-9]+$ ]]; then
-  xresources=$DOTFILES/config/Xresources
-  dpi=$(rg --no-line-number Xft.dpi "$xresources" | awk '{print $2}')
+  xresources=${DOTFILES}/config/Xresources
+  dpi=$(rg --no-line-number Xft.dpi "${xresources}" | awk '{print $2}')
 
-  sed -i "/Xft.dpi/s/$dpi/$1/" "$xresources"
+  sed -i "/Xft.dpi/s/${dpi}/${1}/" "${xresources}"
   echo "Starting the X server with DPI set to $1..."
 
   # Revert back to the default DPI setting after a set time.
-  (sleep 2 && sed -i "/Xft.dpi/s/$1/$dpi/" "$xresources") &
+  (sleep 2 && sed -i "/Xft.dpi/s/${1}/${dpi}/" "${xresources}") &
 
   startx
   exit 0
