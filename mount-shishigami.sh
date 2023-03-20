219c238928e6c799f232856fb25ee1187c47fc18
Oskar Grunning <oskargrunning@gmail.com>
2023-03-20 09:11:30 +0100

    chore: tweak shishigami mount setup

--- mount-shishigami.sh
+++ mount-shishigami.sh
@@ -11,7 +11,7 @@ nc='\033[0m'
 sudo mkdir -p /mnt/shishigami
 sudo mkdir -p /mnt/shishigami/media
 sudo mkdir -p /mnt/shishigami/misc
-sudo mkdir -p /mnt/shishigami/pictures
+sudo mkdir -p /mnt/shishigami/backup
 
 echo -e "${cyan}Mounting shishigami...${nc}"
 
@@ -23,6 +23,6 @@ echo -n 'misc: '
 sudo mount -t nfs -o vers=4 shishigami:/volume1/misc /mnt/shishigami/misc
 echo -e "${green}OK${nc}"
 
-echo -n 'pictures: '
-sudo mount -t nfs -o vers=4 shishigami:/volume1/pictures /mnt/shishigami/pictures
+echo -n 'backup: '
+sudo mount -t nfs -o vers=4 shishigami:/volume1/backup /mnt/shishigami/backup
 echo -e "${green}OK${nc}"
