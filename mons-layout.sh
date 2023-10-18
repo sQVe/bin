5dfb4ac2607982a4abedcc895ee3015bdf70a85c
Oskar Grunning <oskargrunning@gmail.com>
2023-10-18 15:48:34 +0200

    fix: update mons-layout and add hups layout

--- mons-layout.sh
+++ mons-layout.sh
@@ -4,9 +4,11 @@
 #  ┃┃┃┃ ┃┃┗┫┗━┓   ┃  ┣━┫┗┳┛┃ ┃┃ ┃ ┃
 #  ╹ ╹┗━┛╹ ╹┗━┛   ┗━╸╹ ╹ ╹ ┗━┛┗━┛ ╹
 
-if [[ $(envycontrol --query) == "nvidia" ]]; then
-  case "$1" in
-    mancave)
+gpu_mode=$(envycontrol --query)
+
+case "$1" in
+  mancave)
+    if [[ "${gpu_mode}" == 'integrated' ]]; then
       xrandr \
         --output eDP-1-1 --mode 2560x1600 --pos 2560x0 --rotate normal \
         --output DP-1-1-2 --mode 2560x1440 --pos 0x0 --rotate inverted --primary \
@@ -22,24 +24,28 @@ if [[ $(envycontrol --query) == "nvidia" ]]; then
         --output HDMI-1-1 --off \
         --output HDMI-1-2 --off \
         --output HDMI-1-3 --off
-      ;;
-  esac
-else
-  case "$1" in
-    mancave)
+    else
+      notify-send "Unable to load to mancave layout, switch to integrated GPU."
+    fi
+    ;;
+  hups)
+    if [[ "${gpu_mode}" == 'nvidia' ]]; then
       xrandr \
-        --output eDP-1 --mode 2560x1600 --pos 2560x0 --rotate normal \
-        --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate inverted --primary \
-        --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal \
+        --output DP-0 --mode 3840x1600 --pos 0x0 --rotate normal --primary \
         --output DP-1 --off \
         --output DP-1-1 --off \
+        --output DP-1-2 --off \
+        --output DP-1-3 --off \
         --output DP-2 --off \
-        --output DP-3 --off \
-        --output HDMI-1 --off \
-        --output HDMI-2 --off \
-        --output HDMI-3 --off
-      ;;
-  esac
-fi
+        --output HDMI-0 --off \
+        --output HDMI-1-1 --off \
+        --output HDMI-1-2 --off \
+        --output HDMI-1-3 --off \
+        --output eDP-1-1 --off
+    else
+      notify-send "Unable to load to hups layout, switch to dedicated GPU."
+    fi
+    ;;
+esac
 
 random-wallpaper
