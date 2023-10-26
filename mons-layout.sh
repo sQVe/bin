394a5798673d7d2a3c4595d1831211861256c396
Oskar Grunning <oskargrunning@gmail.com>
2023-10-26 08:05:07 +0200

    fix: add mancave office monitor layout

--- mons-layout.sh
+++ mons-layout.sh
@@ -12,20 +12,16 @@ case "$1" in
       office)
         if [[ "${gpu_mode}" == 'integrated' ]]; then
           xrandr \
-            --output eDP-1-1 --mode 2560x1600 --pos 2560x0 --rotate normal \
-            --output DP-1-1-2 --mode 2560x1440 --pos 0x0 --rotate inverted --primary \
-            --output DP-1-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal \
-            --output DP-0 --off \
+            --output eDP-1 --mode 2560x1600 --pos 2560x0 --rotate normal \
+            --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate inverted --primary \
+            --output DP-1-3 --mode 2560x1440 --pos 0x1440 --rotate normal \
             --output DP-1 --off \
-            --output DP-1-1 --off \
-            --output DP-1-1-1 --off \
-            --output DP-1-2 --off \
-            --output DP-1-3 --off \
+            --output HDMI-1 --off \
             --output DP-2 --off \
-            --output HDMI-0 --off \
-            --output HDMI-1-1 --off \
-            --output HDMI-1-2 --off \
-            --output HDMI-1-3 --off
+            --output HDMI-2 --off \
+            --output DP-3 --off \
+            --output HDMI-3 --off \
+            --output DP-1-1 --off
         else
           notify-send "Unable to load to mancave office layout, switch to integrated GPU."
         fi
@@ -45,6 +41,9 @@ case "$1" in
         else
           notify-send "Unable to load to mancave tv layout, switch to dedicated GPU."
         fi
+        ;;
+      *)
+        notify-send "Unknown mancave layout."
         ;;
     esac
     ;;
