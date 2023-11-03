41c930a6203b791ef085acf63c66e4609b7605d3
Oskar Grunning <oskargrunning@gmail.com>
2023-11-03 09:47:31 +0100

    fix: do not tweak dpms on lock

--- lock.sh
+++ lock.sh
@@ -7,9 +7,6 @@
 # Exit early when already locked.
 pgrep -x i3lock && exit
 
-(sleep 1m && xset dpms force standby) &
-dpms_standby=$!
-
 (sleep 1m && sudo -K) &
 sudo_revoke=$!
 
@@ -19,15 +16,12 @@ gpg_clear=$!
 playerctl pause &
 
 dunstctl set-paused true
-xset dpms 30 30 30
 pkill xcompmgr
 
 i3lock -e -n -c 3c3836
 
 dunstctl set-paused false
-xset dpms 240 240 240
 xcompmgr -c -l 0 -t 0 -r 0 -o 0 &
 
-kill "${dpms_standby}"
 kill "${sudo_revoke}"
 kill "${gpg_clear}"
