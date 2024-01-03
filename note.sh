5a12396f8e15806530b6e6c7ef9c6aaea88fec57
Oskar Grunning <oskargrunning@gmail.com>
2024-01-03 20:46:28 +0100

    chore: repair note bin

--- note.sh
+++ note.sh
@@ -4,7 +4,7 @@
 #  ┃┗┫┃ ┃ ┃ ┣╸
 #  ╹ ╹┗━┛ ╹ ┗━╸
 
-files="$(fd --base-directory "$NOTES" --extension md --max-depth 1)"
+files="$(fd --base-directory "$NOTES" --extension md --max-depth 3)"
 choice="$(fzf --select-1 --query "$*" <<< "$files" || exit)"
 
 [[ -z "$choice" ]] && exit 1
